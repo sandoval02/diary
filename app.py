0cf57980b83f2e0cc480a8a0ddc9350edf55734a
Geoff The Kupal <sandovalgeoff4@gmail.com>
2024-11-20 22:17:39 +0800

    Set up production server with Waitress

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
+from waitress import serve
 import os
 from flask import Flask, render_template
 
 app = Flask(__name__)
 app.secret_key = os.getenv('SECRET_KEY', 'default_secret_key')
+
 @app.route('/')
 def home():
     return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    # Use Waitress to serve the app in production
+    serve(app, host='0.0.0.0', port=5000)
