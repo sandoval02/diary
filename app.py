7d7d21762299569b3a9c95e96cd382928a41c27b
Geoff The Kupal <sandovalgeoff4@gmail.com>
2024-11-20 21:57:09 +0800

    Updated app.py to use SECRET_KEY from environment variables

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
+import os
 from flask import Flask, render_template
 
 app = Flask(__name__)
-
+app.secret_key = os.getenv('SECRET_KEY', 'default_secret_key')
 @app.route('/')
 def home():
     return render_template('index.html')
